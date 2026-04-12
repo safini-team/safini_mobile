6f8de14221314616c1118e24253862b62836fd56
Baigazy Sabyrbekov <baigazy.sabyrbekov_2029@ucentralasia.org>
2026-04-12 14:55:24 +0600

    SAF-62: removed sign-in bypass which i used to check if everything is correct

--- lib/features/common/auth/presentation/cubit/login_cubit.dart
+++ lib/features/common/auth/presentation/cubit/login_cubit.dart
@@ -2,21 +2,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:safini/features/common/auth/data/auth_google_sign_in_service.dart';
 import 'package:safini/features/common/auth/presentation/cubit/login_state.dart';
 
-import 'package:flutter/foundation.dart'; // for debug mode bypass
-
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit(this._googleAuth) : super(const LoginState.initial());
 
   final AuthGoogleSignInService _googleAuth;
 
   Future<void> signInWithGoogle() async {
-    // Debug mode bypass
-    if (kDebugMode) {
-      await Future.delayed(const Duration(milliseconds: 500));
-      emit(state.copyWith(status: LoginStatus.success));
-      return;
-    }
-
     emit(state.copyWith(status: LoginStatus.loading, errorMessage: null));
     try {
       await _googleAuth.signInWithGoogle();
